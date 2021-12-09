76a49b202d652347dcea7c9723369365c913477d
George King <george.w.king@gmail.com>
2021-12-09 15:45:00 -0500

    Update build-openssl.sh.

--- build-openssl.sh
+++ build-openssl.sh
@@ -11,4 +11,11 @@ make # -j8 fails for openssl-1.0.2a; osx 10.10.4 tools (gnu make 3.81).
 
 set +x
 echo
-echo 'build complete; run make install.'
+echo 'Build complete; run make install.'
+echo "Then run: py -c 'import ssl; print(ssl.OPENSSL_VERSION)'"
+
+#https://rvm.io/support/fixing-broken-ssl-certificates
+#cert_file="$( openssl version -d | awk -F'"' '{print $2}' )/cert.pem"
+#mkdir -p "${cert_file%/*}"
+#security find-certificate -a -p /Library/Keychains/System.keychain > "$cert_file"
+#security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain >> "$cert_file"
