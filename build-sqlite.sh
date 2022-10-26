48f7f6dd8aeb9551c8659e5cad283683c57fa6c2
George King <george@theorypractice.dev>
2022-10-26 10:28:54 -0400

    Update build-sqlite.sh.

--- build-sqlite.sh
+++ build-sqlite.sh
@@ -1,38 +1,50 @@
-# Copyright 2011 George King. Permission to use this file is granted in license-gloss.txt.
+#!/bin/sh
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 # Build sqlite with various extensions enabled.
 
-
 set -ex
 
-../configure \
-CC=clang CXX=clang++ \
---enable-session \
-CFLAGS="\
--DSQLITE_DQS=0 \
--DSQLITE_DEFAULT_MEMSTATUS=0 \
--DSQLITE_DEFAULT_WAL_SYNCHRONOUS=1 \
--DSQLITE_ENABLE_EXPLAIN_COMMENTS \
--DSQLITE_ENABLE_GEOPOLY \
--DSQLITE_ENABLE_NULL_TRIM \
--DSQLITE_LIKE_DOESNT_MATCH_BLOBS \
--DSQLITE_OMIT_AUTOINIT \
--DSQLITE_OMIT_DEPRECATED \
--Os"
+# Tried to get ICU extension working with this but linking fails.
+#-DSQLITE_ENABLE_ICU
+#CFLAGS:  -I/opt/homebrew/opt/icu4c/include
+#LDFLAGS: -L/opt/homebrew/opt/icu4c/lib
 
-# Additional recommended options from http://www.sqlite.org/compile.html.
-# These options together improve speed and footprint slightly.
-#-DSQLITE_THREADSAFE=0 \
-#-DSQLITE_MAX_EXPR_DEPTH=0 \
-#-DSQLITE_OMIT_DECLTYPE \
-#-DSQLITE_OMIT_PROGRESS_CALLBACK \
-#-DSQLITE_OMIT_SHARED_CACHE
+cflags=(
+  -DSQLITE_DEFAULT_MEMSTATUS=0
+  -DSQLITE_DEFAULT_WAL_SYNCHRONOUS=1
+  -DSQLITE_DQS=0
+  -DSQLITE_ENABLE_DBSTAT_VTAB
+  -DSQLITE_ENABLE_EXPLAIN_COMMENTS
+  -DSQLITE_ENABLE_FTS5
+  -DSQLITE_ENABLE_GEOPOLY
+  -DSQLITE_ENABLE_NULL_TRIM
+  -DSQLITE_ENABLE_RBU
+  -DSQLITE_ENABLE_RTREE
+  -DSQLITE_LIKE_DOESNT_MATCH_BLOBS
+  -DSQLITE_OMIT_AUTOINIT
+  -DSQLITE_OMIT_DEPRECATED
+  -DSQLITE_ENABLE_SESSION
+  -DSQLITE_ENABLE_PREUPDATE_HOOK
+  -I/opt/homebrew/opt/readline/include
+  -Os
+)
+
+ldflags=(
+  -L/opt/homebrew/opt/readline/lib
+)
+
+export CFLAGS="${cflags[@]}"
+export LDFLAGS="${ldflags[@]}"
+../configure CC=clang CXX=clang++
 
 make clean
-make -j6
+make -j8
 
 set +x
 echo
 printf '\nsqlite version: '
 ./sqlite3 -version
+printf '\ncompile options: '
+./sqlite3 ':memory:' 'PRAGMA compile_options;' | grep ENABLE
 echo 'build complete; run `make install` as necessary.'
