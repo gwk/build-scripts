9220f1d2b26123a07ff601b212b3b7962bd53655
George King <george@theorypractice.dev>
2022-10-26 10:28:19 -0400

    Update ssl-checks.py.

--- ssl-checks.py
+++ ssl-checks.py
@@ -1,14 +1,16 @@
+# Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
+
 from ssl import *
 
-from pithy.fs import *
-from pithy.io import *
+from pithy.fs import path_exists
+from pithy.io import errL
 
 paths = get_default_verify_paths()
 #print(paths)
 
 def check(name):
   val = getattr(paths, name)
-  errFL('{}: {} ? {}', name, val, val and path_exists(val))
+  errL(f'{name}: {val} ? {val and path_exists(val, follow=True)}')
 
 check('cafile')
 check('capath')
